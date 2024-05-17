e38493b128ec605979d868d7fc513e9337e9c254
T3CNEO <neoshon421@gmail.com>
2024-05-17 18:00:45 -0400

    starting on debt popups

--- minigame.gd
+++ minigame.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+@onready var prong = preload("res://debt_popup.tscn");
 var debt = 5
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -11,7 +12,10 @@ func _process(delta: float) -> void:
 	if (debt == 0):
 		Game.DebtDone = true
 		get_tree().change_scene_to_file("res://overworld.tscn")
-		
+	if (Input.is_action_pressed("mouse_left")):
+		var prongs = prong.instantiate();
+		prongs.position = Vector2(250, 555);
+		add_child(prongs);
 		
 
 
